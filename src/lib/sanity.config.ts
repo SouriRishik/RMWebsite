52033bfb344ba9789d01f568dfb73e2348d4a04b
SouriRishik <souri.rishik27@gmail.com>
2025-12-19 14:49:01 +0530

    refactor: update structureTool configuration to use defaults

--- src/lib/sanity.config.ts
+++ src/lib/sanity.config.ts
@@ -4,7 +4,7 @@
 
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
+import { structureTool, type StructureBuilder } from "sanity/structure";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
@@ -17,8 +17,9 @@ export default defineConfig({
 	// Add and edit the content schema in the './sanity/schema' folder
 	schema,
 	plugins: [
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-		structureTool(),
+		structureTool({
+			structure: (S: StructureBuilder) => S.defaults(),
+		}),
 		// Vision is a tool that lets you query your content with GROQ in the studio
 		// https://www.sanity.io/docs/the-vision-plugin
 		visionTool({ defaultApiVersion: apiVersion }),
